c8a1b4ebad32a49fb232a356344e868e1fa3f1f2
Ole Morten Didriksen <code@oledid.com>
2024-03-11 17:06:42 +0100

    Innloggingsknapp

--- Web/wwwroot/scripts/index.vue
+++ Web/wwwroot/scripts/index.vue
@@ -1,6 +1,7 @@
 <template>
 	<div class="text-center mt-5">
-		Hello, world! <i class="fa-solid fa-taxi"></i>
+		<div>Hello, world! <i class="fa-solid fa-taxi"></i></div>
+		<a href="/Access/Login" class="btn btn-primary mt-3">Logg inn</a>
 	</div>
 </template>
 <script lang="ts" setup>
