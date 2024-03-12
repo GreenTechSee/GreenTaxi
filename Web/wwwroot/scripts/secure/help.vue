bebc8081771b178ad00ff3f3eb9b69df96fc65bb
Alida Hjelljord <hjelljord.alida@gmail.com>
2024-03-12 19:02:42 +0100

    Help

--- Web/wwwroot/scripts/secure/help.vue
+++ Web/wwwroot/scripts/secure/help.vue
@@ -1,6 +1,23 @@
 <template>
-	<div class="container">
-		<h1>Veiledning</h1>
+	<div class="container mb-5">
+		<h1 class="text-center">INFO</h1>
+		<div class="d-flex justify-content-center mt-3">
+			<div>
+				<iframe 
+					src="https://www.youtube.com/embed/e75m35UZW3E?si=ZWfsv9WE8QCirihZ" 
+					title="YouTube video player" 
+					frameborder="0" 
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+					allowfullscreen>
+				</iframe>
+				<div class="text-center"><small>Video: hjerte- og lungeredning</small></div>
+			</div>
+		</div>
+		<div class="dropup position-absolute bottom-0 end-0 rounded-circle" style="z-index: 100; margin-bottom: 100px; margin-right: 10px;">
+			<button type="button" class="btn btn-primary btn-lg rounded-circle" aria-label="Chat" style="height: 60px; width: 60px;">
+				<i class="fa-solid fa-robot"></i>
+			</button>
+		</div>
 	</div>
 </template>
 <script lang="ts" setup>
