8bf0d3ef336a47048f8590c84557aef6140c05af
Alida Hjelljord <hjelljord.alida@gmail.com>
2024-03-13 11:23:41 +0100

    better dash

--- Web/wwwroot/scripts/index.vue
+++ Web/wwwroot/scripts/index.vue
@@ -1,16 +1,17 @@
 <template>
+	<topNav></topNav>
 	<div class="container mt-5">
 		<h1>SafeHaven</h1>
 		<p>Velkommen til ditt innloggings-dashboard</p>
 		<div class="row">
-			<div class="col-3 border rounded p-5 m-3 text-center">
+			<div class="col border rounded p-5 m-3 text-center text-nowrap">
 				I dag er det<br />
 				{{ moment().format("DD.MM.YYYY") }}
 			</div>
-			<div class="col-3 border rounded p-5 m-3 text-center">
+			<div class="col border rounded p-5 m-3 text-center">
 				<a href="/Access/Login" class="btn btn-primary mt-3">Logg inn</a>
 			</div>
-			<div class="col-3 border rounded p-5 m-3 text-center">
+			<div class="col border rounded p-5 m-3 text-center text-nowrap">
 				Klokken er<br />
 				{{ timeStr }}
 			</div>
@@ -18,6 +19,7 @@
 	</div>
 </template>
 <script lang="ts" setup>
+import topNav from "./components/topNav.vue";
 import moment from "moment";
 import { ref } from "vue";
 
